c1cdf442ff36056a9eeb672a078b6fb131098a92
NDH <nghai2k@gnail.com>
2025-01-02 16:56:17 +0700

    1

--- app/Admin/Controllers/ApiUIController.php
+++ app/Admin/Controllers/ApiUIController.php
@@ -5072,10 +5072,10 @@ class ApiUIController extends AdminController
 
         // $count = InfoCongDoan::whereNull('order_id')->count();
         // return $count;
-        $infos = InfoCongDoan::with('tem')->whereNull('order_id')->chunkById(100, function ($infos) {
+        $infos = InfoCongDoan::with('oldPlan')->whereNull('order_id')->chunkById(100, function ($infos) {
             foreach ($infos as $info) {
-                if ($info->tem) {
-                    $info->update(['order_id' => $info->tem->order_id]);
+                if ($info->oldPlan) {
+                    $info->update(['order_id' => $info->oldPlan->order_id ?? null]);
                 }
             }
         });

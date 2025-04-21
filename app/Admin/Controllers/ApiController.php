82b428a7b1d46fc3ea48e554ce8edd0bbd55a80d
NDH <nghai2k@gnail.com>
2025-04-21 09:28:07 +0700

    1

--- app/Admin/Controllers/ApiController.php
+++ app/Admin/Controllers/ApiController.php
@@ -8191,8 +8191,6 @@ class ApiController extends AdminController
                 ->from('warehouse_mlt_logs')
                 ->groupBy('material_id');
         })->orderByraw('CHAR_LENGTH(material_id) DESC')->orderBy('material_id');
-        $query->whereDate('warehouse_mlt_logs.tg_nhap', '>=', '2025-01-01')
-            ->whereDate('warehouse_mlt_logs.tg_nhap', '<=', date('Y-m-d'));
         if (isset($input['loai_giay']) || isset($input['kho_giay']) || isset($input['dinh_luong']) || isset($input['ma_cuon_ncc']) || isset($input['ma_vat_tu']) || isset($input['so_kg']) || isset($input['so_cuon'])) {
             $query->whereHas('material', function ($q) use ($input) {
                 if (isset($input['loai_giay'])) $q->where('loai_giay', 'like', "%" . $input['loai_giay'] . "%");
